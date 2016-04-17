8988fb94454e3274056554303064bb9dd70cee9a
Morning <jiangcx099@126.com>
2016-04-17 18:05:47 +0800

    delete

--- tests/encjpg/regmdl_gen.sh
+++
@@ -1,9 +0,0 @@
-###################################################################
-# File Name:					regmdl_gen.sh
-# Author: 					 	Morning
-# mail: 						jiangcx099@126.com
-# Created Time: 				Wed 14 Oct 2015 04:30:38 PM CST
-###################################################################
-
-#!/bin/bash
-ralgen -t topname -I dir -uvm ralf
